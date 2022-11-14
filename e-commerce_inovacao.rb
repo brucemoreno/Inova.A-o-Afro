cb2d89f86eacc5d61d78d0a51050b227c553978b
brucemoreno <brucemoreno90@gmail.com>
2022-11-13 21:29:13 -0300

    Update e-commerce_inovacao.rb

--- e-commerce_inovacao.rb
+++ e-commerce_inovacao.rb
@@ -1,51 +1,42 @@
 class ECOM
 
-    def initialize
-        
-
-        menu_principal()
+    def initialize       
+     menu_principal()
     end
 
    def valores
-        @carrinho = 0
+        @carrinho = @carrinho.to_f
         @produtoA = 50.00
         @produtoB = 200.00
         @produtoC = 100.00
    end
 
     def compras
-        valores()                   
+        valores()               
         puts "Selecione a opção desejada:"
         puts "[1] Produto A: R$#{@produtoA}"
         puts "[2] Produto B: R$#{@produtoB}"
-        puts "[3] Produto C: R$#{@produtoC}"       
+        puts "[3] Produto C: R$#{@produtoC}"
+        puts "[4] Voltar ao menu principal"       
         puts "Subtotal R$#{@carrinho}"
         puts "----------------------------------"
-    
-        input = gets.chomp.to_i
-
-       
+        input = gets.chomp.to_i     
         case input
-        when 1
-           
-                
-            
+        when 1          
             puts "Digite a quantidade desejada:"
             multiplicador = gets.chomp.to_i
-            @carrinho += (multiplicador * @produtoA)
-            
+            @carrinho += (multiplicador * @produtoA)            
         when 2
             puts "Digite a quantidade desejada:"
             multiplicador = gets.chomp.to_i
-            carrinho += (multiplicador * produtoB)        
-            
-            
+            @carrinho += (multiplicador * @produtoB)           
         when 3
             puts "Digite a quantidade desejada:"
             multiplicador = gets.chomp.to_i
-            carrinho += (multiplicador * produtoC)                
-        end
-       
+            @carrinho += (multiplicador * @produtoC)
+        when 4
+            menu_principal()                
+        end       
             puts "Seu subtotal é de #{@carrinho}"
             puts "-------------------------------------------"
             puts "Pressione 0 para voltar ao menu de compras"
@@ -57,12 +48,8 @@ class ECOM
                 puts "Pressione 0 para voltar ao menu de compras"
             end
             
+        end         
         end
-            
-            
-      
-        end
-       
     end
 
     def menu_principal
@@ -75,20 +62,10 @@ class ECOM
     when 1
         compras()
     when 2
-        puts "Até"
+        puts "Até breve!!!"
     else
         puts "Seleção inválida escolha entre a opção número 1 ou número 2"
     end
   end
-
-
-def error_message
-    puts "Comando Inválido, pressione 0 para voltar ao menu anterior"
-    input = gets.chomp.to_i
-    unless input == 0
-        puts "Comando Inválido, pressione 0 para voltar ao menu anterior"
-    end
-end
-
 
 ECOM.new
